574ceff43fefe245ecfa9a64e19626ae5f7b623b
Y-Akinci <yaren-can@hotmail.ch>
2025-10-06 12:23:19 +0200

    Anlage data_preperation: Datenimport und Überprüfung

---
+++ Data Preparation/data_preparation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+dateipfad = r"C:\Users\User\Documents\GitHub\Forecast & Anomalie Detection\251006_Stromverbrauch Basel 2012-2025.csv"
+
+df = pd.read_csv(dateipfad, sep = ";")
+
+print("Anzahl Zeilen und Spalten:", df.shape)
+print("Spaltennamen:", df.columns.tolist())
+#print(df.head())
+
+df["Start der Messung"] = pd.to_datetime(df["Start der Messung"], errors = "coerce")
+df = df.set_index("Start der Messung")
+
+print(df.index.dtype)
+#print(df.head(3))
+
+# Erwartetes Zeitraster (alle 15 Minuten)
+start = df.index.min()
+ende = df.index.max()
+
+# Alle Zeitpunkte, die theoretisch vorhanden sein sollten
+voller_index = pd.date_range(start=start, end=ende, freq="15T", tz="UTC")
+
+# Fehlende Zeitpunkte
+fehlende = voller_index.difference(df.index)
+
+# Doppelte Zeitpunkte
+doppelte = df.index[df.index.duplicated()]
+
+print("Zeitraum:", start, "bis", ende)
+print("Sollte haben:", len(voller_index), "Zeitpunkte")
+print("Hat tatsächlich:", len(df.index))
+print("Fehlende Zeitpunkte:", len(fehlende))
+print("Doppelte Zeitpunkte:", len(doppelte))
+
+# Fehlende Zeitpunkte anzeigen
+print("Erste 10 fehlende Zeitpunkte:")
+print(fehlende[:10])
+
+# Optional: Differenzen zwischen Zeitpunkten prüfen
+delta = df.index.to_series().diff().value_counts().head(5)
+print("\nHäufigste Zeitabstände:")
+print(delta)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Energy-Forecasting-Anomaly-Detection-Basel
 Machine-Learning-Modell zur Vorhersage und Anomalieerkennung im Energieverbrauch.
+
+Orientierung CRISP-DM-Zyklus
+Business Understanding
+Data Understanding
+Data Preperation Modeling
+Evaluation
+Deployment
