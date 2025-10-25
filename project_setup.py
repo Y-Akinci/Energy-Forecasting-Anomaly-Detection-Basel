db9dcf43fa6599c2aaffe0243978b80c0ed9ee13
Y-Akinci <yaren-can@hotmail.ch>
2025-10-25 16:43:43 +0200

    Delete project_setup.py

--- project_setup.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-
-def add_project_root():
-    """Fügt den Projekt-Hauptordner zum Python-Pfad hinzu."""
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    if root_dir not in sys.path:
-        sys.path.insert(0, root_dir)
