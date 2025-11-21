8d417266ae5e7ff6fc0ab1645f9d52abe525ecce
Rem <kerem240hotmail.com>
2025-11-21 18:21:47 +0100

    Data Preparation Formatting

--- Data_Preparation/data_preparation.py
+++ Data_Preparation/data_preparation.py
@@ -1,6 +1,6 @@
 import os, sys
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
 
@@ -12,7 +12,7 @@ import seaborn as sns
 # Pfade
 TZ = "Europe/Zurich"
 ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-DATA_DIR = os.path.join(ROOT, "data")
+DATA_DIR = os.path.join(ROOT, "Energy-Forecasting-Anomaly-Detection-Basel", "data")
 OUT_15 = os.path.join(DATA_DIR, "merged_strom_meteo_15min.csv")
 
 # === 1) STROM laden (helpers -> Europe/Zurich) und auf UTC bringen ===
@@ -52,6 +52,8 @@ def make_ts_utc(df):
 
 w1["DateTime"] = make_ts_utc(w1)
 w2["DateTime"] = make_ts_utc(w2)
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 w1 = w1.dropna(subset=["DateTime"]).set_index("DateTime").sort_index()
 w2 = w2.dropna(subset=["DateTime"]).set_index("DateTime").sort_index()
@@ -96,11 +98,97 @@ print("Duplikate:", merged_15.index.duplicated().sum())  # 0
 full_idx = pd.date_range(merged_15.index.min(), merged_15.index.max(), freq="15min", tz="UTC")
 print("Fehlende Slots:", len(full_idx.difference(merged_15.index)))  # 0 oder nur echte Datenlücken
 
-
 # === 5) Export ===
 #merged_15.to_csv(OUT_15, sep=";", encoding="utf-8")
 #print("OK ->", OUT_15)
 #print("Rows:", len(merged_15), "Cols:", merged_15.shape[1])
 
 # === DATA PREPROCCESSING ===
+
+# === DATA CLEANING ===
+
+# Zeilen löschen, welche keinen Wert bei der Zielvariable "Stromverbrauch" haben
+merged_15.dropna(subset="Stromverbrauch", inplace=True)
+
+# === DATATYPES ===
+
+
+# Datentyp ändern, integers
+int_cols = [
+    "Jahr", "Monat", "Tag", "Wochentag", "Tag des Jahres",
+    "Quartal", "Woche des Jahres",
+]
+
+for col in int_cols:
+    merged_15[col] = pd.to_numeric(
+        merged_15[col], errors="coerce"
+    ).astype("Int64") 
+
+# Datentypen ändern, float64
+float_cols = [
+    'Stromverbrauch',
+    'Grundversorgte Kunden',
+    'Freie Kunden',
+    'Globalstrahlung; Zehnminutenmittel',
+    'Diffusstrahlung; Zehnminutenmittel',
+    'Langwellige Einstrahlung; Zehnminutenmittel',
+    'Sonnenscheindauer; Zehnminutensumme',
+    'BÃ¶enspitze (3-SekundenbÃ¶e); Maximum in km/h',
+    'BÃ¶enspitze (3-SekundenbÃ¶e); Maximum in m/s',
+    'BÃ¶enspitze (SekundenbÃ¶e); Maximum in km/h',
+    'Böenspitze (Sekundenböe)',
+    'Chilltemperatur',
+    'Dampfdruck 2 m ü. Boden',
+    'Luftdruck reduziert auf Meeresniveau (QFF)',
+    'Luftdruck reduziert auf Meeresniveau mit Standardatmosphäre (QNH)',
+    'Luftrdruck auf Barometerhöhe',
+    'Lufttemperatur 2 m ü. Boden',
+    'Lufttemperatur 2 m ü. Gras',
+    'Lufttemperatur Bodenoberfläche',
+    'Niederschlag; Zehnminutensumme',
+    'Taupunkt 2 m ü. Boden',
+    'Windgeschwindigkeit skalar; Zehnminutenmittel in m/s',
+    'Windgeschwindigkeit vektoriell',
+    'Windgeschwindigkeit; Zehnminutenmittel in km/h',
+    'Windrichtung; Zehnminutenmittel',
+    'relative Luftfeuchtigkeit'
+]
+
+for col in float_cols:
+    merged_15[col] = pd.to_numeric(merged_15[col], errors="coerce").astype("float64")
+
+# Datentypen für KAtegoriale variablen
+categorical_features = [
+    "Jahr",
+    "Monat",
+    "Tag",
+    "Wochentag",
+    "Quartal",
+    "Woche des Jahres"
+]
+
+for col in categorical_features:
+    merged_15[col] = merged_15[col].astype("category")
+
+# === FEATURE SELECTION ===
+
+# Tag, Monat, und Jahr entfernt
+merged_15.drop(["Jahr", "Monat","Tag"], axis=1)
+
 # === FEATURE ENGINEERING ===
+
+# "Date" und "Time" als eigene Spalten erstellen
+merged_15.reset_index(inplace=True)
+merged_15["Date"] = merged_15["index"].dt.strftime("%Y-%m-%d")
+merged_15["Time"] = merged_15["index"].dt.strftime("%H-%M-%S")
+# Spalte mit dem Tag im Jahr kreieren
+merged_15['DayOfYear'] = merged_15["index"].dt.dayofyear
+merged_15.set_index("index", inplace=True)
+
+# Datentyp ändern, Zeitwerte
+merged_15["Date"] = pd.to_datetime(merged_15["Date"], format="%Y-%m-%d")
+merged_15["Time"] = pd.to_datetime(merged_15["Time"], format="%H-%M-%S")
+
+# Spalte mit dem Tag im Jahr kreieren
+
+print(merged_15.head())
