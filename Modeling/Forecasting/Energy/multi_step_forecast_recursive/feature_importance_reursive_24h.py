3b91ff1ec3eee2f434cfa3af376a26ced9cbad91
Y-Akinci <yaren-can@hotmail.ch>
2025-12-31 14:19:06 +0100

    anpassung abstände multi step modell

--- Modeling/Forecasting/Energy/multi_step_forecast_recursive/feature_importance_reursive_24h.py
+++ Modeling/Forecasting/Energy/multi_step_forecast_recursive/feature_importance_reursive_24h.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import joblib
-
 from pathlib import Path
 
 warnings.filterwarnings("ignore", message="X does not have valid feature names")
