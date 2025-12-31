40980471b742280016f138bd39630abea1c63292
Y-Akinci <yaren-can@hotmail.ch>
2025-12-31 14:17:28 +0100

    anpassung main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 import os, sys
-import subprocess
 
-# --- Stelle sicher, dass Projekt-Root korrekt im Pfad ist ---
+# Projekt-Root korrekt im Pfad ist
 root_dir = os.path.dirname(os.path.abspath(__file__))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
